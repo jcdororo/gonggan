92f13b6586ef211f58e07253ddc922c20475dea4
jc <wnwlcks123@gmail.com>
2023-11-15 13:54:41 +0900

    공통 CSS 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
-import Image from 'next/image'
+import { Roboto } from "next/font/google"
+
+const roboto = Roboto({
+  subsets: ['latin'],
+  weight: ['400', '700']
+})
 
 export default function Home() {
   return (
-    <div>
-      레이아웃
+    <div className={roboto.className}>
+      <button >버튼</button>
+      <input type='text' value='텍스트박스'></input>
     </div>
   )
 }
