53b6be9a165795eb4fc31e1cf1558854f4ebab34
jc <wnwlcks123@gmail.com>
2023-11-17 14:32:48 +0900

    문의목록 ui 수정

--- src/app/admin/contact/list/page.tsx
+++ src/app/admin/contact/list/page.tsx
@@ -1,7 +1,85 @@
 export default function ContactList() {
   return (
     <div>
-      문의목록 입니다.
+      <div className='text-center font-extrabold text-2xl my-4'>문의 목록</div>
+
+      <div className="max-w-6xl mx-auto mt-12 text-center">
+        <div className="bg-white p-4 shadow-lg">
+          <table className="w-full">
+
+            <thead>
+              <tr className="h-14 border-b-4">
+                <th className="py-2 w-96">내용</th>
+                <th className="py-2 w-48">이메일</th>
+                <th className="py-2 w-44 ">날짜</th>
+                <th className="py-2 w-28 ">상태</th>
+              </tr>
+            </thead>
+
+            <tbody className="">
+
+              <tr className="h-10 hover:underline">
+                <td className="py-2">Lorem Ipsum is simply dummy text Lorem Ipsum is simply dummy text Lorem Ipsum is simply dummy text </td>
+                <td className="py-2">example@example.com</td>
+                <td className="py-2">2023-11-17</td>
+                <td className="py-2">진행 중</td>
+              </tr>
+
+              <tr className="h-10 hover:underline">
+                <td className="py-2">Lorem Ipsum is simply dummy text Lorem Ipsum is simply dummy text Lorem Ipsum is simply dummy text </td>
+                <td className="py-2">example@example.com</td>
+                <td className="py-2">2023-11-17</td>
+                <td className="py-2">진행 중</td>
+              </tr>
+
+              <tr className="h-10 hover:underline">
+                <td className="py-2">Lorem Ipsum is simply dummy text Lorem Ipsum is simply dummy text Lorem Ipsum is simply dummy text </td>
+                <td className="py-2">example@example.com</td>
+                <td className="py-2">2023-11-17</td>
+                <td className="py-2">진행 중</td>
+              </tr>
+
+              <tr className="h-10 hover:underline">
+                <td className="py-2">Lorem Ipsum is simply dummy text Lorem Ipsum is simply dummy text Lorem Ipsum is simply dummy text </td>
+                <td className="py-2">example@example.com</td>
+                <td className="py-2">2023-11-17</td>
+                <td className="py-2">진행 중</td>
+              </tr>
+
+              <tr className="h-10 hover:underline">
+                <td className="py-2">Lorem Ipsum is simply dummy text Lorem Ipsum is simply dummy text Lorem Ipsum is simply dummy text </td>
+                <td className="py-2">example@example.com</td>
+                <td className="py-2">2023-11-17</td>
+                <td className="py-2">진행 중</td>
+              </tr>
+
+              <tr className="h-10 hover:underline">
+                <td className="py-2">Lorem Ipsum is simply dummy text Lorem Ipsum is simply dummy text Lorem Ipsum is simply dummy text </td>
+                <td className="py-2">example@example.com</td>
+                <td className="py-2">2023-11-17</td>
+                <td className="py-2">진행 중</td>
+              </tr>
+
+              <tr className="h-10 hover:underline">
+                <td className="py-2">Lorem Ipsum is simply dummy text Lorem Ipsum is simply dummy text Lorem Ipsum is simply dummy text </td>
+                <td className="py-2">example@example.com</td>
+                <td className="py-2">2023-11-17</td>
+                <td className="py-2">진행 중</td>
+              </tr>
+
+              <tr className="h-10 hover:underline">
+                <td className="py-2">Lorem Ipsum is simply dummy text Lorem Ipsum is simply dummy text Lorem Ipsum is simply dummy text </td>
+                <td className="py-2">example@example.com</td>
+                <td className="py-2">2023-11-17</td>
+                <td className="py-2">진행 중</td>
+              </tr>
+
+            </tbody>
+          </table>
+
+        </div>
+        <div className="my-12 font-semibold text-lg">1 2 3 4 5 6 7 ...</div>
+      </div>
     </div>
   )
 }
