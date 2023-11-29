76c32c7289657ec07ffb4314f2a231828d24b0b5
정화윤 <jeonghwayun0119@gmail.com>
2023-11-29 13:02:10 +0900

    장소 상세화면 생성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,31 @@
-import { Roboto } from "next/font/google"
+"use client";
+import { Roboto } from "next/font/google";
+import { useState } from "react";
+
+import DetailModal from "./detail/DetailModal";
 
 const roboto = Roboto({
-  subsets: ['latin'],
-  weight: ['400', '700']
-})
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
 
 export default function Home() {
+  const [detail, setDetail] = useState(false);
+
+  const handleModal:
+    | React.MouseEventHandler<HTMLDivElement>
+    | undefined = (): void => {
+      setDetail(!detail);
+  };
+
   return (
     <div className={roboto.className}>
-      <button >버튼</button>
-      <input type='text' value='텍스트박스'></input>
+      <button>버튼</button>
+      <input type="text" value="텍스트박스"></input>
+      <div className="" onClick={handleModal}>
+        클릭
+      </div>
+      {detail ? <DetailModal handleModal={handleModal} /> : ""}
     </div>
-  )
+  );
 }
