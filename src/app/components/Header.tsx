0b22b8c74e92b1c40e0b9a69eef70c08d2a3ee4b
jc <wnwlcks123@gmail.com>
2023-11-30 15:16:54 +0900

    next config Image 설정

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,15 +1,29 @@
 import Image from "next/image";
 import header from '/public/logo.png'
+import { authOptions } from "@/pages/api/auth/[...nextauth].js"
+import { getServerSession } from "next-auth"
 
-export default function Header() {
+export default async function Header() {
+  let session: any = await getServerSession(authOptions)
+  console.log('session',session.user.image)
   return (
-    <div className="h-20 bg-white relative">
+    <div className="h-20 bg-white fixed">
       <Image 
-        className="absolute m-1"
+        className="m-1 relative"
         src={header}
         width={110}
         height={75}
-        alt="header"/>
+        alt="header"
+      />
+      
+      <div className="right-1">
+        <Image 
+          src={session.user.image}
+          width={40}
+          height={40}
+          alt='아이콘'
+        />
+      </div>
     </div>
   )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header />
+          <Header />
         {children}
       </body>
     </html>
