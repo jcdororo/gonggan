c2d6e8688d0d3cbe4b9ff62bae4e9d640e8f3c88
jc <wnwlcks123@gmail.com>
2024-01-19 15:27:25 +0900

    404페이지 라우팅 추가

--- src/app/admin/contact/list/[id]/page.tsx
+++ src/app/admin/contact/list/[id]/page.tsx
@@ -1,8 +1,15 @@
 import ContactDetail from "@/app/components/Admin/ContactDetail";
 import Title from "@/app/components/Title";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import { getServerSession } from "next-auth";
+import { notFound } from "next/navigation";
 
-export default function Detail ({ params }: { params: { id: string } }) {
+export default async function Detail ({ params }: { params: { id: string } }) {
   const id = params?.id;
+  const session: any = await getServerSession(authOptions);
+  if(session.user.role != 'admin') {
+    notFound();
+  }
 
   return (
     <>
